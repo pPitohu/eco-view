60d00fc4f6aa7f021f36e4644eb269ec8b731bbb
pPitohu <59769228+pPitohu@users.noreply.github.com>
2023-01-13 04:44:35 +0300

    try-fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -29,10 +29,10 @@ app.use(router)
 
 mongoose.connect(
   process.env.MONGO_URL,
-  // {
-  //   useNewUrlParser: true,
-  //   useUnifiedTopology: true,
-  // }
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 ).then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err))
   
